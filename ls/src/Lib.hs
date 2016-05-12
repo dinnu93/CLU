50965e15bd797727af6c2d0a8ba562189ba499b9
dinnu93 <dinesh.padisetti@gmail.com>
2016-05-12 22:35:58 +0530

    Added ls -a command

--- ls/src/Lib.hs
+++ ls/src/Lib.hs
@@ -10,21 +10,45 @@ import Data.Char as C
 lsMain :: IO ()
 lsMain = do
   args <- getArgs
-  dirs <- D.listDirectory "."
-  putStrLn $ argsDispatch args dirs
+  dirList <- D.getDirectoryContents "."
+  putStrLn $ argsDispatch args dirList
   
 type Args = [String]
 type Option = String 
- 
+
+-- argsDispatch :: dispatches relevant operations to arguments              
 argsDispatch :: Args -> [String] -> String
 argsDispatch args dirList
-  | length args == 0 = showDirCloumns . dirSort . rmDotFiles $ dirList -- Simple ls Command 
+  | length args == 0 = showDirCloumns . dirSort . rmDotFiles $ dirList -- Simple ls Command without any options
+  | length args == 1 && optionCheck o1 = optionsDispatch o1 $ dirList -- ls [option]
+  where o1 = (args !! 0)
+        o2 = (args !! 1)
 
+-- optionsDispatch :: dispatches relevant functionality for options
+optionsDispatch :: Option -> [String] -> String
+optionsDispatch o
+  | o == "-a" || o == "--all" = showDirCloumns . dirSort  
+
+-- dirSort :: sorts the files and directories in alphabetical order
 dirSort :: [String] -> [String]
-dirSort = L.sortBy (\x y -> (map C.toLower x) `compare` (map C.toLower y))
+dirSort = L.sortBy (\x y -> (map C.toLower (rmDot x)) `compare` (map C.toLower (rmDot y)))
 
+--removes dot infront of a string if it has one
+rmDot :: String -> String
+rmDot s
+  | head s == '.' = tail s
+  | otherwise = s
+    
+-- rmDotFiles :: removes the hidden and ignored files and directories which start
+-- with a dot
 rmDotFiles :: [String] -> [String] 
 rmDotFiles = filter (\x -> not (head x == '.'))
 
+
+-- showDirCloumns :: displays all the files and directories in columns  
 showDirCloumns :: [String] -> String
 showDirCloumns = L.intercalate " " 
+
+-- optionCheck :: checks if the given argument is an option or not
+optionCheck :: String -> Bool
+optionCheck o = (head o) == '-'
